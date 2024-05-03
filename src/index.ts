4afbcfa1922043187a9f5a68558de2791f79672f
mabanana <aldwin.yu@hotmail.com>
2024-05-03 15:27:25 -0400

    Add c.r.u.d functions that trigger through telegram message

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {
   HttpRequest,
   HttpResponse,
   Config,
+  Sqlite,
 } from "@fermyon/spin-sdk";
 
 const TELEGRAM_API_URL = "https://api.telegram.org";
@@ -48,11 +49,100 @@ export const handleRequest: HandleRequest = async function (
       status: 200,
     };
   }
+
   const message = body.message.text;
 
-  await sendTextMessage(message + " from crud-list bot", chatId, botToken);
+  createTable();
+
+  if (message.startsWith("/add ")) {
+    addMessage(message.slice(5));
+    sendTextMessage("Message added.", chatId, botToken);
+  } else if (message == "/list") {
+    const messages = (await getMessages()).rows;
+    if (!Array.isArray(messages) || messages.some((m) => !m.id || !m.message)) {
+      throw new Error("Invalid messages format");
+    }
+
+    let messageList = "Messages:\n";
+
+    for (let i = 0; i < messages.length; i++) {
+      messageList += `ID: ${messages[i].id}, Message: ${messages[i].message}\n`;
+    }
+    if (messages.length === 0) {
+      messageList += "No messages found.";
+    }
+    await sendTextMessage(messageList, chatId, botToken);
+  } else if (message.startsWith("/delete ")) {
+    const id = parseInt(message.slice(8));
+    deleteMessage(id);
+    sendTextMessage("Message deleted.", chatId, botToken);
+  } else if (message.startsWith("/update ")) {
+    const parts = message.slice(8).split(" ");
+    if (parts.length < 2 || isNaN(parseInt(parts[0]))) {
+      await sendTextMessage(
+        "Invalid format. Please use '/update id new_message'.",
+        chatId,
+        botToken
+      );
+    } else {
+      const id = parseInt(parts[0]);
+      const newMessage = parts.slice(1).join(" ");
+      console.log(id, newMessage);
+      updateMessage(id, newMessage);
+      sendTextMessage("Message updated to: " + newMessage, chatId, botToken);
+    }
+  } else if (message.startsWith("/deletetable")) {
+    deleteTable();
+    sendTextMessage("Table deleted.", chatId, botToken);
+  } else {
+    await sendTextMessage("Invalid command.", chatId, botToken);
+  }
 
   return {
     status: 200,
   };
 };
+
+function createTable() {
+  let conn = Sqlite.openDefault();
+  conn.execute(
+    "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, message TEXT)",
+    []
+  );
+}
+
+function deleteTable() {
+  let conn = Sqlite.openDefault();
+  conn.execute("DROP TABLE messages", []);
+}
+
+function addMessage(message: string) {
+  let conn = Sqlite.openDefault();
+  let id = Date.now();
+  conn.execute("INSERT INTO messages (id, message) VALUES (?, ?)", [
+    id,
+    message,
+  ]);
+}
+
+async function getMessages() {
+  let conn = Sqlite.openDefault();
+  return await conn.execute("SELECT * FROM messages", []);
+}
+
+function deleteMessage(id: number) {
+  let conn = Sqlite.openDefault();
+  conn.execute("DELETE FROM messages WHERE id = ?", [id]);
+}
+
+function updateMessage(id: number, newMessage: string) {
+  let conn = Sqlite.openDefault();
+  const result = conn.execute("SELECT * FROM messages WHERE id = ?", [id]);
+  if (!result) {
+    throw new Error("Message not found.");
+  }
+  conn.execute("UPDATE messages SET message = ? WHERE id = ?", [
+    newMessage,
+    id,
+  ]);
+}
