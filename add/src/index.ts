eaeb864984c308259e4957b1f7d89c888a605f41
mabanana <aldwin.yu@hotmail.com>
2024-05-08 19:04:00 -0400

    Add /add endpoint logic

--- add/src/index.ts
+++ add/src/index.ts
@@ -1,9 +1,39 @@
-import { HandleRequest, HttpRequest, HttpResponse } from "@fermyon/spin-sdk"
+import {
+  HandleRequest,
+  HttpRequest,
+  HttpResponse,
+  Sqlite,
+} from "@fermyon/spin-sdk";
 
-export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
+export const handleRequest: HandleRequest = async function (
+  request: HttpRequest
+): Promise<HttpResponse> {
+  const request_body: any = request.json();
+  const message = request_body.message;
+
+  if (message === undefined) {
+    return {
+      status: 200,
+      headers: { "content-type": "text/plain" },
+      body: "No Message Found.",
+    };
+  }
+  console.log("Message: ", message);
+  let conn = Sqlite.openDefault();
+  conn.execute(
+    "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, message TEXT)",
+    []
+  );
+
+  let id = Date.now();
+  conn.execute("INSERT INTO messages (id, message) VALUES (?, ?)", [
+    id,
+    message,
+  ]);
+
   return {
     status: 200,
     headers: { "content-type": "text/plain" },
-    body: "Hello from TS-SDK"
-  }
-}
+    body: "Message Added Successfully.",
+  };
+};
